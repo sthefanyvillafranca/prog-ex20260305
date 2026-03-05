29a6fb49c873bb2901cc0cb945e4d31bce75f267
sthefanyvillafranca <svillsei@myuax.com>
2026-03-05 08:51:32 +0100

    feat(CentroFP): creación de atributos privados y metodos constructor con inicialización de array

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -1,4 +1,12 @@
 package PACKAGE_NAME;
 
 public class CentroFP {
+    private Alumno[] alumnos;
+    private final int MAX_ALUMNOS;
+
+    public CentroFP(int MAX_ALUMNOS) {
+        this.MAX_ALUMNOS = 20;
+        alumnos = new Alumno[MAX_ALUMNOS];
+    }
+
 }
