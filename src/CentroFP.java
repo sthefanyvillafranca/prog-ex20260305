319b101dc5ce1840d17f1a326cfe3f067226ced2
sthefanyvillafranca <svillsei@myuax.com>
2026-03-05 08:55:21 +0100

    feat(CentroFP): Creación de método que busca hueco libre

--- src/CentroFP.java
+++ src/CentroFP.java
@@ -9,4 +9,17 @@ public class CentroFP {
         alumnos = new Alumno[MAX_ALUMNOS];
     }
 
+    private int buscarPrimerHuecoLibre(){
+        int posicionHuecoLibre = -1;
+        boolean sigueBuscando = true;
+
+        for(int i = 0; i < MAX_ALUMNOS && sigueBuscando; i++){
+            if(alumnos[i] == null){
+                posicionHuecoLibre = i;
+                sigueBuscando = false;
+            }
+        }
+        return posicionHuecoLibre;
+    }
+
 }
