7c2e50f084290feba0cebd534e3562884e1a4b1c
sthefanyvillafranca <svillsei@myuax.com>
2026-03-05 08:47:08 +0100

    feat(Alumno) creación de atributos privados, método constructor, métodos getters y toString()

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,4 +1,36 @@
 package PACKAGE_NAME;
 
 public class Alumno {
+    private int id;
+    private String nombre;
+    private String tituloFP;
+    private int edad;
+
+    public Alumno(int id, String nombre, String tituloFP, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tituloFP = tituloFP;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTituloFP() {
+        return tituloFP;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Alumno [id: %d, nombre: %s, tituloFP: %s, edad: %d]", id, nombre, tituloFP, edad);
+    }
 }
